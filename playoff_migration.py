ce1d65ebd48b3061a919fb20aaec10c82771ea74
MisterWeeMan <mattia21bano@gmail.com>
2018-06-29 09:35:57 +0200

    refactor game selection

--- playoff_migration.py
+++ playoff_migration.py
@@ -41,8 +41,15 @@ class PlayoffMigration(object):
     # ++++++++++++++++++++++++
     # UTILITIES
 
-    # TODO : is there a way to make this private?
-    def get_number_pages(self, number):
+    def __get_game(self, game: Games):
+        """ Return instance of the chosen game """
+        if game == Games.original:
+            return self._original
+        elif game == Games.cloned:
+            return self._cloned
+
+    @staticmethod
+    def __get_number_pages(number):
         """ Returns the number of pages needed for pagination """
         n_pages = int(number / 100)
 
@@ -54,31 +61,30 @@ class PlayoffMigration(object):
     # ++++++++++++++++++++++++
     # INFORMATION RETRIEVERS
 
-    def get_game_id(self, game: Games) -> str:
-        """ Returns game id of the chosen game """
-        if game == Games.original:
-            return self._original.get('/admin')["game"]["id"]
-        elif game == Games.cloned:
-            return self._cloned.get('/admin')["game"]["id"]
-
     def get_number_teams(self, game: Games):
         """ Returns the number of teams of the chosen game """
-        if game == Games.original:
-            return self._original.get('/admin/teams', {})['total']
-        elif game == Games.cloned:
-            return self._cloned.get('/admin/teams', {})['total']
+        return self.__get_game(game).get('/admin/teams', {})['total']
+
+    def get_number_players(self, game: Games):
+        """ Returns the number of players in the chosen game """
+        return self.__get_game(game).get('/admin/players', {})['total']
+
+    def get_number_players_in_team(self, game: Games, team_key):
+        """ Returns the number of players in the chosen game """
+        return self.__get_game(game).get('/admin/teams/' + team_key + '/members', {})['total']
+
+    def get_game_id(self, game: Games) -> str:
+        """ Returns game id of the chosen game """
+        return self.__get_game(game).get('/admin')["game"]["id"]
 
     def get_teams_by_id(self, game: Games):
         """ Returns all the teams ids  """
-        teams = {}
         teams_id = {}
         count_key = 0
+        game_instance = self.__get_game(game)
 
-        for count in range(self.get_number_pages(self.get_number_teams(game))):
-            if game == Games.original:
-                teams = self._original.get('/admin/teams', {"skip": str(count * 100), "limit": "100"})
-            elif game == Games.cloned:
-                teams = self._cloned.get('/admin/teams', {"skip": str(count * 100), "limit": "100"})
+        for count in range(PlayoffMigration.__get_number_pages(self.get_number_teams(game))):
+            teams = game_instance.get('/admin/teams', {"skip": str(count * 100), "limit": "100"})
 
             for item in teams['data']:
                 teams_id.update({count_key: item['id']})
@@ -86,24 +92,14 @@ class PlayoffMigration(object):
 
         return teams_id
 
-    def get_number_players(self, game: Games):
-        """ Returns the number of players in the chosen game """
-        if game == Games.original:
-            return self._original.get('/admin/players', {})['total']
-        elif game == Games.cloned:
-            return self._cloned.get('/admin/players', {})['total']
-
     def get_players_by_id(self, game: Games):
         """ Returns all the ids of the player in the chosen game """
         players_id = {}
         count_key = 0
-        players = {}
+        game_instance = self.__get_game(game)
 
-        for count in range(self.get_number_pages(self.get_number_players(game))):
-            if game == Games.original:
-                players = self._original.get('/admin/players', {"skip": str(count * 100), "limit": "100"})
-            elif game == Games.cloned:
-                players = self._cloned.get('/admin/players', {"skip": str(count * 100), "limit": "100"})
+        for count in range(PlayoffMigration.__get_number_pages(self.get_number_players(game))):
+            players = game_instance.get('/admin/players', {"skip": str(count * 100), "limit": "100"})
 
             for item in players['data']:
                 players_id.update({count_key: item['id']})
@@ -111,29 +107,18 @@ class PlayoffMigration(object):
 
         return players_id
 
-    def get_number_players_in_team(self, game: Games, team_key):
-        """ Returns the number of players in the chosen game """
-        if game == Games.original:
-            return self._original.get('/admin/teams/' + team_key + '/members', {})['total']
-        elif game == Games.cloned:
-            return self._cloned.get('/admin/teams/' + team_key + '/members', {})['total']
-
     def get_players_by_teams(self, game: Games):
         """ Returns all the players grouped by each team of the selected game """
         teams_by_id = self.get_teams_by_id(game)
         players_by_teams = {}
 
         for key in teams_by_id:
-            players_in_team = {}
             count_key = 0
+            game_instance = self.__get_game(game)
 
-            for count in range(self.get_number_pages(self.get_number_players_in_team(game, teams_by_id.get(key)))):
-                if game == Games.original:
-                    players_in_team = self._original.get('/admin/teams/' + teams_by_id.get(key) + '/members',
-                                                         {"skip": str(count * 100), "limit": "100"})
-                elif game == Games.cloned:
-                    players_in_team = self._cloned.get('/admin/teams/' + teams_by_id.get(key) + '/members',
-                                                       {"skip": str(count * 100), "limit": "100"})
+            for count in range(PlayoffMigration.__get_number_pages(self.get_number_players_in_team(game, teams_by_id.get(key)))):
+                players_in_team = game_instance.get('/admin/teams/' + teams_by_id.get(key) + '/members',
+                                                    {"skip": str(count * 100), "limit": "100"})
 
                 pl_team = {}
                 for item in players_in_team['data']:
@@ -154,5 +139,6 @@ quindi solo se eseguo "python playoff_migration.py"
 """
 if __name__ == '__main__':
     p = PlayoffMigration()
-    pprint(p.get_players_by_teams(Games.original))
+    pprint(p.get_game_id(Games.original))
+    pprint(p.get_game_id(Games.cloned))
 
