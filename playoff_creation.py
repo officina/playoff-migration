7d95d438bdf3c3b776ae9f55d5e1d41bcd9c8263
MisterWeeMan <mattia21bano@gmail.com>
2018-07-05 18:00:14 +0200

    Added import methods

--- playoff_creation.py
+++ playoff_creation.py
@@ -5,7 +5,6 @@ import json
 from pprint import pprint
 
 
-
 class PlayoffCreation(object):
 
     file_path: str
@@ -21,55 +20,6 @@ class PlayoffCreation(object):
             type='client',
             allow_unsecure=True
         )
-    def get_teams_design(self):
-        """ Return teams design of chosen game """
-        return self.target.get('/design/versions/latest/teams', {})
-
-    @staticmethod
-    def __get_number_pages(number):
-        """ Returns the number of pages needed for pagination """
-        n_pages = int(number / 100)
-
-        if number % 100 > 0:
-            n_pages += 1
-
-        return n_pages
-
-    def get_teams_by_id(self):
-        """ Returns all the teams ids  """
-        teams_id = {}
-        count_key = 0
-        number_teams = self.target
-        number_pages = PlayoffMigration.__get_number_pages(number_teams)  # pagination management
-
-        for count in range(number_pages):
-            teams = self.target.get('/admin/teams', {"skip": str(count * 100), "limit": "100"})
-
-            for item in teams['data']:
-                teams_id.update({count_key: item['id']})
-                count_key += 1
-
-        return teams_id
-
-    def get_number_players(self):
-        """ Returns the number of players in the chosen game """
-        return self.target.get('/admin/players', {})['total']
-
-    def get_players_by_id(self):
-        """ Returns all the ids of the player in the chosen game """
-        players_id = {}
-        count_key = 0
-        number_players = self.get_number_players(target)
-        number_pages = PlayoffMigration.__get_number_pages(number_players)  # pagination management
-
-        for count in range(number_pages):
-            players = game_instance.get('/admin/players', {"skip": str(count * 100), "limit": "100"})
-
-            for item in players['data']:
-                players_id.update({count_key: item['id']})
-                count_key += 1
-
-        return players_id
 
     def get_leaderboards_by_id(self):
         """ Returns leaderboards by id of the selected game """
@@ -88,27 +38,6 @@ class PlayoffCreation(object):
     def get_metrics_design_id(self):
         """ Returns metrics design id """
         return self.target.get("/design/versions/latest/metrics", {})
-
-
-    def delete_teams_design(self):
-        """ Delete team designs in chosen game """
-        teams_design = self.get_teams_design()
-
-        for team in teams_design:
-            self.target.delete('/design/versions/latest/teams/' + team['id'], {})
-
-    def delete_teams_instances(self):
-        """ Delete teams instances in chosen game """
-        teams_instance = self.get_teams_by_id(target)
-
-        for team in teams_instance:
-            self.target.delete('/admin/teams/' + teams_instance.get(team), {})
-
-    def delete_player_instances(self):
-        """ Deletes all the player instances from the selected game"""
-        players_instance = self.get_players_by_id(target)
-        for player in players_instance:
-            self.target.delete('/admin/players/' + players_instance.get(player), {})
 
     def delete_actions_design(self):
         """ Delete actions design in chosen game """
@@ -131,33 +60,108 @@ class PlayoffCreation(object):
         for item in metrics_design_id:
             self.target.delete("/design/versions/latest/metrics/" + item['id'], {})
 
+    # OK METHOD
 
     def import_teams_design(self):
         self.delete_teams_design()
-        teams_design = {}
+
         with open(self.file_path + "teamsDesign.json", "r") as file:
             teams_design = json.load(file)
-            pprint(teams_design)
 
-        for key, value in teams_design.items() :
-            pprint(key)
+        for key, value in teams_design.items():
             self.target.post("/design/versions/latest/teams", {}, value)
 
     def import_teams_instance(self):
+        self.delete_teams_instances()
+
         with open(self.file_path + "teamsInstances.json", "r") as file:
             teams_instance = json.load(file)
-            for key, value in teams_instance.items() :
-                try:
-                    control = self.target.get('/admin/teams/' + key, {})
-                    if control is not None:
-                        self.target.delete('/admin/teams/' + key, {})
-                except PlayoffException as err:
-                    if not err.message == "Team " + key + " not found":
-                        self.target.delete('/admin/teams/' + key, {})
-                    else:
-                        pprint(err.message)
 
-                self.target.post("/admin/teams", {}, value)
+        for key, value in teams_instance.items():
+            self.target.post("/admin/teams", {}, value)
+
+    def import_players(self):
+        self.delete_player_instances()
+
+        with open(self.file_path + "players.json", "r") as file:
+            players = json.load(file)
+
+        for key, value in players.items():
+            self.target.post('/admin/players', {}, value)
+
+    def delete_teams_design(self):
+        """ Delete team designs in chosen game """
+        teams_design = self.get_teams_design()
+
+        for team in teams_design:
+            self.target.delete('/design/versions/latest/teams/' + team['id'], {})
+
+    def delete_teams_instances(self):
+        """ Delete teams instances in chosen game """
+        teams_instance = self.get_teams_by_id()
+
+        for team in teams_instance:
+            self.target.delete('/admin/teams/' + teams_instance.get(team), {})
+
+    def delete_player_instances(self):
+        """ Deletes all the player instances from the selected game"""
+        players_instance = self.get_players_by_id()
+
+        for player in players_instance:
+            self.target.delete('/admin/players/' + players_instance.get(player), {})
+
+    @staticmethod
+    def __get_number_pages(number):
+        """ Returns the number of pages needed for pagination """
+        n_pages = int(number / 100)
+
+        if number % 100 > 0:
+            n_pages += 1
+
+        return n_pages
+
+    def get_number_teams(self):
+        """ Returns the number of teams of the chosen game """
+        return self.target.get('/admin/teams', {})['total']
+
+    def get_teams_by_id(self):
+        """ Returns all the teams ids  """
+        teams_id = {}
+        count_key = 0
+        number_teams = self.get_number_teams()
+        number_pages = self.__get_number_pages(number_teams)  # pagination management
+
+        for count in range(number_pages):
+            teams = self.target.get('/admin/teams', {"skip": str(count * 100), "limit": "100"})
+
+            for item in teams['data']:
+                teams_id.update({count_key: item['id']})
+                count_key += 1
+
+        return teams_id
+
+    def get_teams_design(self):
+        """ Return teams design of chosen game """
+        return self.target.get('/design/versions/latest/teams', {})
+
+    def get_number_players(self):
+        return self.target.get('/admin/players', {})['total']
+
+    def get_players_by_id(self):
+        """ Returns all the ids of the player in the chosen game """
+        players_id = {}
+        count_key = 0
+        number_players = self.get_number_players()
+        number_pages = self.__get_number_pages(number_players)  # pagination management
+
+        for count in range(number_pages):
+            players = self.target.get('/admin/players', {"skip": str(count * 100), "limit": "100"})
+
+            for item in players['data']:
+                players_id.update({count_key: item['id']})
+                count_key += 1
+
+        return players_id
 
     # def import_player_instances(self):
     #     with open(self.file_path + "teamsInstances.json", "r") as file:
